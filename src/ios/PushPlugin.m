43907759cd138721bdbf704d4e7c1728a4ba94cb
Martin Arnberg <marnberg@schoolmessenger.com>
2013-09-17 15:38:04 -0700

    Escape common character for iOS in app notification display

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -212,7 +212,16 @@
         if ([thisObject isKindOfClass:[NSDictionary class]])
             [self parseDictionary:thisObject intoJSON:jsonString];
         else
-            [jsonString appendFormat:@"%@:'%@',", key, [inDictionary objectForKey:key]];
+            NSMutableString *s = [NSMutableString stringWithString:[inDictionary objectForKey:key]];
+            [s replaceOccurrencesOfString:@"\'" withString:@"\\\'" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"/" withString:@"\\/" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"\n" withString:@"\\n" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"\b" withString:@"\\b" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"\f" withString:@"\\f" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"\r" withString:@"\\r" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+            [s replaceOccurrencesOfString:@"\t" withString:@"\\t" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [s length])];
+        
+            [jsonString appendFormat:@"%@:'%@',", key, s];
     }
 }
 
